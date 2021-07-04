6f78445e3fe9f576989a2f695d3e7fdae48a1a26
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-07-03 20:51:17 -0600

    Added About and Prices components

--- src/components/GeneralPrices.jsx
+++ src/components/GeneralPrices.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import xqc1 from '../css/pricingImages/xqc1.jpg';
+import jotaro from '../css/pricingImages/jotaro.jpg';
+import pogchamp from '../css/pricingImages/pogchamp.png';
 
 export default function GeneralPrices() {
     return (
-        <div>
-            
-        </div>
+        <section className="generalprices-wrapper">
+            <div className="header-text-container">
+                <h1 className="header-text">General Pricing</h1>
+                <h2 className="subheader-text">Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. Dolorum, corrupti!
+                </h2>
+            </div>
+            <div className="pricing">
+                <div className="pricing-type-one">
+                    <div className="pricing-image">
+                        <img src={xqc1} alt="xqc"/>
+                    </div>
+                    <div className="pricing-text"> Lorem ipsum dolor sit amet 
+                    consectetur adipisicing elit. Voluptates similique quo
+                     maiores magni incidunt quam error, aliquid eveniet omnis id.
+                     </div>
+                </div>
+
+                <div className="pricing-type-two">
+                    <div className="pricing-text">
+                        Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Ipsa, soluta?
+                    </div>
+                    <div className="pricing-image">
+                        <img src={jotaro} alt="jotaro"/>
+                    </div>
+                </div>
+
+                <div className="pricing-type-three">
+                    <div className="pricing-image">
+                    <img src={pogchamp} alt="pogchamp"/>
+                    </div>
+                    <div className="pricing-text">
+                        Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Nesciunt, officia?
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
 export default function Portfolio() {
+    let portfolioImages = '../css/portfolioimages';
     return (
-        <div>
+        <section className="portfolio-wrapper">
+            <div className="portfolio-introduction">
+                <div className="portfolio-introduction-text-box">
+
+                </div>
+                <div className="portfolio-introduction-image">
+
+                </div>
+            </div>
+            <div className="portfolio-gallery">
+                {
+            
+            portfolioImages.forEach((image, index) => {
+                //create a div element with grid classname, then append it to portfolio-gallery
+                
+            })
+                }
+            </div>
             
             
-        </div>
+            
+            
+        </section>
     )
 }
