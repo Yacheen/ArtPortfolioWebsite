56c5b442f4b5bbd12a450af928c2678178dfecc1
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-07-11 00:26:57 -0600

    Completed ThemeChanger and footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,19 +15,18 @@ export default function NavBar({}) {
     const [isToggled, setIsToggled] = useState(false);
     const setTheme = color => {
         localStorage.setItem('theme', color);
-        document.documentElement.className = color;
+        document.documentElement.className = `${color} !important`
     }
     const handleClick = () => {
-        if(localStorage.getItem('theme') === 'blue-theme') {
+        if(isToggled) {
             setTheme('green-theme');
         }else {
             setTheme('blue-theme');
         }
     }
     
-    
-    
     return (
+    
         <Router>
             <nav className="nav-bar">
                 <div className="nav-bar-content">
@@ -35,7 +34,8 @@ export default function NavBar({}) {
                         <h2><Link className="Home-link" to="/">Alanna</Link></h2>
                     </div>
                     <ul className="navigation-links">
-                        <li><ThemeChanger onClick={handleClick()} rounded={true} isToggled={isToggled} onToggle={() => {
+                        <li><p>Theme</p>
+                            <ThemeChanger onClick={handleClick()} rounded={true} isToggled={isToggled} onToggle={() => {
                                 setIsToggled(!isToggled);
                                 console.log(isToggled);
                             }}/>
@@ -46,6 +46,8 @@ export default function NavBar({}) {
                 </div>
             </nav>
 
+            
+
             <Switch>
             <Route path="/" exact={true} component={Home}/>
                 
@@ -53,7 +55,20 @@ export default function NavBar({}) {
         
             <Route path="/Contact" exact={true} component={Contact} />
             </Switch>
+
+            <footer className="footer">
+                <div className="footer-content">
+                    <div className="footer-copyright-text">
+                        <p>Alanna &#169; All rights reserved</p>
+                    </div>
+                    <div className="footer-links">
+                        <Link className="footer-link" to="/General-Prices"><p>Prices</p></Link>
+                        <Link className="footer-link" to="/Contact"><p>Contact</p></Link>
+                    </div>
+                </div>
+            </footer>
         </Router>
+
 
     )
 }
