7bcedcec8a9afd19e405674588c224362600aff1
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-07-14 09:09:59 -0600

    Edited Material UI Nav Bar

--- src/components/NavigationTabs.jsx
+++ src/components/NavigationTabs.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import PhoneIcon from './PhoneIcon';
-import ShoppingCartIcon from './ShoppingCartIcon';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import PaletteIcon from '@material-ui/icons/Palette';
+import { Link } from 'react-router-dom';
 
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    maxWidth: 500,
+    backgroundColor: "#0b2e2d",
+    fontSize: "12px",
+    
+    
+  },
+});
 
-export default function NavigationTabs() {
-    return (
-        <Paper square>
-        <Tabs
-          variant="fullWidth"
-          indicatorColor="secondary"
-          textColor="secondary"
-          aria-label="icon label tabs example"
-        >
-          <Tab icon={<ShoppingCartIcon />} label="Pricing" />
-          <Tab icon={<PhoneIcon />} label="Contact" />
-        </Tabs>
-      </Paper>
-    )
+export default function IconLabelTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper square className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        aria-label="icon label tabs example"
+      >
+        <Link className="nav-buttons" to="/" ><Tab className={classes.root} icon={<PaletteIcon  fontSize="large"/>} label="PORTFOLIO" /></Link>
+        <Link className="nav-buttons" to="/General-Prices" ><Tab className={classes.root} icon={<LocalOfferIcon fontSize="large"/>} label="PRICING" /></Link>
+        <Link className="nav-buttons" to="/Contact" ><Tab className={classes.root} icon={<ChatBubbleIcon fontSize="large"/>} label="CONTACT" /></Link>
+        
+      </Tabs>
+    </Paper>
+  );
 }
